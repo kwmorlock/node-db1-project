99ef4e8fae09288a469dda77c5b04ded47816cb4
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-12 01:28:10 -0400

    second get

--- posts/customerRouter.js
+++ posts/customerRouter.js
@@ -15,3 +15,21 @@ router.get("/", (req, res) => {
         res.status(500).json({ message: error.messsage });
       });
   });
+
+  router.get("/:id", (req, res) => {
+    db("accounts")
+      .where({ id: req.params.id })
+      .first() 
+      .then(post => {
+        if (post) {
+          res.status(200).json({ data: post });
+        } else {
+          res.status(404).json({ message: "No posts by that ID" });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: error.messsage });
+      });
+  });
+  
