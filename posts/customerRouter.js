2270091f4cd5db0b15489bb854da087d96fbd3e3
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-12 01:19:14 -0400

    get added

--- posts/customerRouter.js
+++ posts/customerRouter.js
@@ -3,3 +3,15 @@ const express = require("express");
 const db = require("../data/db-config.js");
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+    db.select("accounts")
+      .from("accounts")
+      .then(accounts => {
+        res.status(200).json({ data: accounts });
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: error.messsage });
+      });
+  });
