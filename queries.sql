4f34827f0467af3b7acdd6cd93e6e677c8158809
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-12 00:10:25 -0400

    first 10 orders queries

--- queries.sql
+++ queries.sql
@@ -16,6 +16,11 @@ where supplierId = 11;
 
 -- List first 10 orders placed, sorted descending by the order date
 
+select * 
+from orders 
+order by orderDate desc
+limit 10;
+
 -- Find all customers that live in London, Madrid, or Brazil
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
